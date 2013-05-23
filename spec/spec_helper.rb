57aca11912cd2f0de2e6a0b7369369c4e1094bc8
Jonathan Claudius <claudijd@yahoo.com>
2013-05-23 16:23:05 -0400

    Make SimpleCov optional

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,7 @@
-require 'simplecov'
-SimpleCov.start
+begin
+  require 'simplecov'
+  SimpleCov.start
+rescue LoadError
+  warn "Couldn't load simplecov, so coverage reports will be generated"
+  warn "Run 'gem install simplecov' to get coverage reports"
+end
