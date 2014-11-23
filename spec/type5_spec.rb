1eee9700a13c80f8ef02117be9934be1ce5c9ff6
Jonathan Claudius <jclaudius@trustwave.com>
2014-11-22 20:33:39 -0500

    Add specs for another password

--- spec/type5_spec.rb
+++ spec/type5_spec.rb
@@ -12,7 +12,20 @@ describe C7Decrypt::Type5 do
   
     subject{@hash}
     its(:class) {should == ::String}
-    it {should == "$1$TMnL$iAFs16ZXx7x18vR1DeIp6/"}
+    it {should == "$1$#{@salt}$iAFs16ZXx7x18vR1DeIp6/"}
   end
+
+  context "when encrypting single Cisco Type-5 hash" do 
+    before(:each) do
+      @password = "Password123"
+      @salt = "VkQd"
+      @hash = C7Decrypt::Type5.encrypt(@password, @salt)
+    end
+  
+    subject{@hash}
+    its(:class) {should == ::String}
+    it {should == "$1$#{@salt}$Vma3sR7B1LL.v5lgy1NYc/"}
+  end
+
 
 end
