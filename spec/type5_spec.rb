7e357acdda50324f88b3fd7349e56e6e08756126
Jonathan Claudius <jclaudius@mozilla.com>
2016-03-12 10:51:37 -0500

    Fix rspec errors

--- spec/type5_spec.rb
+++ spec/type5_spec.rb
@@ -3,29 +3,29 @@ require 'rspec/its'
 
 describe C7Decrypt::Type5 do
 
-  context "when encrypting single Cisco Type-5 hash" do 
+  context "when encrypting single Cisco Type-5 hash" do
     before(:each) do
       @password = "SECRETPASSWORD"
       @salt = "TMnL"
       @hash = C7Decrypt::Type5.encrypt(@password, @salt)
     end
-  
-    subject{@hash}
-    its(:class) {should == ::String}
-    it {should == "$1$#{@salt}$iAFs16ZXx7x18vR1DeIp6/"}
+
+    it "should be the right class and value" do
+      expect(@hash).to be_kind_of(::String)
+      expect(@hash).to eql("$1$#{@salt}$iAFs16ZXx7x18vR1DeIp6/")
+    end
   end
 
-  context "when encrypting single Cisco Type-5 hash" do 
+  context "when encrypting single Cisco Type-5 hash" do
     before(:each) do
       @password = "Password123"
       @salt = "VkQd"
       @hash = C7Decrypt::Type5.encrypt(@password, @salt)
     end
-  
-    subject{@hash}
-    its(:class) {should == ::String}
-    it {should == "$1$#{@salt}$Vma3sR7B1LL.v5lgy1NYc/"}
+
+    it "should be the right class and value" do
+      expect(@hash).to be_kind_of(::String)
+      expect(@hash).to eql("$1$#{@salt}$Vma3sR7B1LL.v5lgy1NYc/")
+    end
   end
-
-
 end
